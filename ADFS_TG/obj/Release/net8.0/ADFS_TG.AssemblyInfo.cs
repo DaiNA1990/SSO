770844d3a2b5627e0dc6bf37e07a18e23e5b2bf1
Anh Đài <daina@fpt.com>
2025-02-17 17:18:08 +0700

    update

--- ADFS_TG/obj/Release/net8.0/ADFS_TG.AssemblyInfo.cs
+++ ADFS_TG/obj/Release/net8.0/ADFS_TG.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ADFS_TG")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e6caf7b59ec31881e2961dc0e1423f9cb2c4dd8")]
 [assembly: System.Reflection.AssemblyProductAttribute("ADFS_TG")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ADFS_TG")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
